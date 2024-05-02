cc36c859e371d878b7643f9b91e68bc307c65170
Monika Singh <moninom@amazon.com>
2024-05-02 06:33:10 +0000

    Remove endianess conflict between stun and rtp

--- source/include/rtp_endianness.h
+++ source/include/rtp_endianness.h
@@ -5,14 +5,14 @@
 #include <stdint.h>
 
 /* Endianness Function types. */
-typedef void ( * WriteUint32_t ) ( uint8_t * pDst,
-                                   uint32_t val );
-typedef uint32_t ( * ReadUint32_t ) ( const uint8_t * pSrc );
+typedef void ( * RtpWriteUint32_t ) ( uint8_t * pDst,
+                                      uint32_t val );
+typedef uint32_t ( * RtpReadUint32_t ) ( const uint8_t * pSrc );
 
 typedef struct RtpReadWriteFunctions
 {
-    WriteUint32_t writeUint32Fn;
-    ReadUint32_t readUint32Fn;
+    RtpWriteUint32_t writeUint32Fn;
+    RtpReadUint32_t readUint32Fn;
 } RtpReadWriteFunctions_t;
 
 void Rtp_InitReadWriteFunctions( RtpReadWriteFunctions_t * pReadWriteFunctions );
